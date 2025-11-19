6f84fb56fd9086cb44d7fd3f3f3a3fcdf54370b3
Edilson SIlva <81516823+dhjosep1998@users.noreply.github.com>
2025-11-19 08:49:15 -0300

    ALtera titulo

--- tests/test_home.py
+++ tests/test_home.py
@@ -6,7 +6,7 @@ def test_home(browser):
     url = "http://127.0.0.1:8081"
     browser.get(url)
     time.sleep(10)
-    assert browser.title == "Dashboard"
+    assert browser.title == "Dash"
     print("Teste da paginal inicial feito com sucesso")
 
 
